4ba71a5c2c75c0a0cfe62cc76ccaf111e2009b97
Missy Davies <ms.melissadavies@gmail.com>
2021-03-01 03:17:15 +0000

    Added largest integer function

--- lists.py
+++ lists.py
@@ -110,7 +110,12 @@ def largest_int(numbers):
         True
     """
 
-    return 0
+    sorted_numbers = sorted(numbers)
+    
+    if len(sorted_numbers) == 0:
+        return None
+    else:
+        return sorted_numbers[-1]
 
 
 def halvesies(numbers):
